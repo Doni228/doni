7ec561ed9b44c2c007a910d8a41d32877dd0fcf7
Doni228 <50043064+Doni228@users.noreply.github.com>
2019-07-05 22:09:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const Discord = require('discord.js'); 
 const user = new Discord.Client(); 
 user.login(process.env.token);
+
+user.on('ready', () => {
+user.user.setActivity('за сервером Mesa', { type: "WATCHING" });
+});
