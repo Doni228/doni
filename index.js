d10a6edfe5b000ec8d34e6d4a1781434be75127e
Doni228 <50043064+Doni228@users.noreply.github.com>
2019-11-06 09:43:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const Discord = require('discord.js'); 
-const user = new Discord.Client(); 
-user.login(process.env.token);
 
-user.on('ready', () => {
-user.user.setActivity('за тобой', { type: "WATCHING" });
-});
